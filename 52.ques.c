6e405ef45b18fa1d34e05599d8af058f26ea0c08
Riddhi Mittal <riddhimittal13rm@gmail.com>
2025-09-15 16:17:40 +0530

    Add files via upload

---
+++ 52.ques.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("*\n\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+    
+    printf("*\n");
+
+    return 0;
+}
