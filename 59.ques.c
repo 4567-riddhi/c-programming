997b361b01836bf38dde7d3604a988faafc6a90f
Riddhi Mittal <riddhimittal13rm@gmail.com>
2025-09-19 14:45:21 +0530

    Add files via upload

---
+++ 59.ques.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int evenCount = 0, oddCount = 0;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+    printf("Even=%d, Odd=%d\n", evenCount, oddCount);
+
+    return 0;
+}
---
+++ 60.ques.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int posCount = 0, negCount = 0, zeroCount = 0;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++) {
+        if(arr[i] > 0)
+            posCount++;
+        else if(arr[i] < 0)
+            negCount++;
+        else
+            zeroCount++;
+    }
+    printf("Positive=%d, Negative=%d, Zero=%d\n", posCount, negCount, zeroCount);
+
+    return 0;
+}
