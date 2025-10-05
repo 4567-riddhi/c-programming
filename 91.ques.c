95466c86001760d9fbe674e454553852606e2614
Riddhi Mittal <riddhimittal13rm@gmail.com>
2025-10-05 13:46:45 +0530

    Add files via upload

---
+++ 91.ques.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], result[100];
+    int i = 0, j = 0;
+    printf("Enter a string: ");
+    scanf("%s", str);  
+    while (str[i] != '\0') {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u' &&
+            str[i] != 'A' && str[i] != 'E' && str[i] != 'I' &&
+            str[i] != 'O' && str[i] != 'U') {
+            result[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    result[j] = '\0';  
+    printf("Output: %s\n", result);
+
+    return 0;
+}
